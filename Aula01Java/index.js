b2da7ce37d56f25d9dfa00d1b94802a730b7507d
Emanuelo1 <emanuelsempremengao@gmail.com>
2023-11-17 13:23:51 -0300

    update

--- Aula01Java/index.js
+++ Aula01Java/index.js
@@ -18,10 +18,23 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
+app.get('/users/add', (req, res) => {
+    res.render('userform')
+    })
+    
+    app.post('/users/save', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    
+    const user = { name: name, age: age }
+    res.render('viewuser', { user: user })
+    
+    })
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server online')
   })
